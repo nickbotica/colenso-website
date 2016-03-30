8d53124644bf3ce7e26ce3cc5c0b57d60f3921a3
nickbotica <nick.botica@outlook.com>
2016-03-31 02:30:38 +1300

    Searc and open files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+// basex
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("OPEN Colenso");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/",function(req, res){
+	res.render('index', { title: 'Colenso'});
+});
+
+
+router.get('/search', function(req, res) {
+	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+		"for $p in *[.//text() contains text '" + req.query.searchString + "'] return db:path($p)",
+		function(err, result) {
+			if(!err){
+				list = result.result.split("\n");
+				numResults = list.length;
+				res.render('search', { title: 'Search results', content: req.query.searchString, list: list, numResults: numResults});
+			}});
+});
+
+
+router.get("/load",function(req,res){
+	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+		"(doc('Colenso/"+req.query.file+"'))[1]",
+		function (error, result) {
+			if(!error){
+				res.render('load', { title: req.query.file, file: result.result });
+			}
+		});
 });
 
 module.exports = router;
